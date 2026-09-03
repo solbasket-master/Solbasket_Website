e1d72d39113c17a8ee8d1a99aa2a88525f992420
gayu-0603 <gayathriganesan0603@gmail.com>
2026-09-03 11:39:00 +0530

    Website fixation

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: './',
+  server: {
+    port: 5173,
+    host: true
+  }
 })
+
+
